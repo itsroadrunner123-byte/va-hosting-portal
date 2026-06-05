4e60fde00e83d17a579a4bfa5b013ae3b38d651e
Sean <its.road.runner@gmail.com>
2026-06-05 00:45:03 +0000

    Fix: Correctly expose db and models in models package init

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,6 @@
-# Make the models directory a Python package
+# Make the models directory a Python package and expose db and models
+# Import db and models from the actual models file within this package
+from .models import db, User, Package, ServerConfig, Ticket
+
+__all__ = ['db', 'User', 'Package', 'ServerConfig', 'Ticket']
 __version__ = '0.1.0'
